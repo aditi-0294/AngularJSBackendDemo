6ad5b86270f52bfee9bf4479c5a672923bb4355a
Aditi <a.aditi0294@rediffmail.com>
2017-08-26 02:52:39 +0530

    second commit : contains request mapping for getting person details by id .

--- backendCrudDemo/src/main/java/com/niit/controller/PersonController.java
+++ backendCrudDemo/src/main/java/com/niit/controller/PersonController.java
@@ -6,6 +6,7 @@ import javax.ws.rs.GET;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,7 +21,7 @@ public class PersonController {
 	private PersonDao personDao ; // loose-coupling
 	
 	
-	
+	// get all person details
 	@RequestMapping(value = "/person/getAllPersons" , method = RequestMethod.GET)
 	public @ResponseBody List<Person> getAllPersons() { 
 		// @ResponseBody returns data in JSON format along with status code
@@ -31,5 +32,14 @@ public class PersonController {
 		return persons ;
 	}
 	
+	
+	// get person details by id
+	@RequestMapping(value = "/person/getPersonById/{id}" , method = RequestMethod.GET)
+	public @ResponseBody Person getPersonById(@PathVariable int id) {
+		
+		Person person = personDao.getPersonById(id) ;
+		return person ;
+		
+	}
 	
 }
