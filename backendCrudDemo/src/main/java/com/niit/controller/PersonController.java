ed560d3c74bcae18773a0ec3451415260c3e6501
Aditi <a.aditi0294@rediffmail.com>
2017-08-26 22:15:11 +0530

    third commit : request mapping to insert and save person details into database

--- backendCrudDemo/src/main/java/com/niit/controller/PersonController.java
+++ backendCrudDemo/src/main/java/com/niit/controller/PersonController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import javax.ws.rs.GET;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.niit.dao.PersonDao;
 import com.niit.model.Person;
@@ -21,7 +24,7 @@ public class PersonController {
 	private PersonDao personDao ; // loose-coupling
 	
 	
-	// get all person details
+	// get all person details :
 	@RequestMapping(value = "/person/getAllPersons" , method = RequestMethod.GET)
 	public @ResponseBody List<Person> getAllPersons() { 
 		// @ResponseBody returns data in JSON format along with status code
@@ -33,13 +36,23 @@ public class PersonController {
 	}
 	
 	
-	// get person details by id
+	// get person details by id :
 	@RequestMapping(value = "/person/getPersonById/{id}" , method = RequestMethod.GET)
 	public @ResponseBody Person getPersonById(@PathVariable int id) {
 		
 		Person person = personDao.getPersonById(id) ;
 		return person ;
 		
+	}
+	
+	
+	// Insert and save person details :
+	@RequestMapping(value = "/person/insertSavePersonDetails" , method = RequestMethod.POST)
+	//@ResponseStatus(value = HttpStatus.NO_CONTENT) // PostMan Status = 204 (NoContent)
+	@ResponseStatus(value = HttpStatus.OK)
+	public void insertPersonDetails(@RequestBody Person person) {
+		
+		personDao.insertPersonDetails(person);
 	}
 	
 }
