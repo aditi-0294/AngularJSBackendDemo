baff1584783d801a5e00496c009f3d0249890274
Aditi <a.aditi0294@rediffmail.com>
2017-08-27 23:03:27 +0530

    second commit : function call

--- SPA_Demo/WebContent/js/PersonViewController.js
+++ SPA_Demo/WebContent/js/PersonViewController.js
@@ -6,25 +6,40 @@ myApp.controller('PersonViewController' , function($scope,PersonService,$locatio
 	
 	var id = $routeParams.id ;
 	
-	// to get person details by Id on clicking link :
 	
+	// to get person details by Id on clicking link :
 	PersonService.getPersonById(id).then(function(response) {
 		
 		$scope.person = response.data ; // success codes - list of array details which retrieved from POSTMAN
-		console.log("Controller.js success status : " + response.status)
-		console.log("Controller.js success data : " + response.data)
+		console.log("Controller.js getPersonById() success status : " + response.status)
+		console.log("Controller.js getPersonById() success data : " + response.data)
 		//$location.path('/getPersonById')  ;
 		//console.log("Controller.js path reached : " + $location.path())
 		 
 	} , function(response) {
 		
 		$scope.person = response.data ; // error / failure codes - list of array details which retrieved from POSTMAN
-		console.log("Controller failure / error status : " + response.status) 
-		console.log("Controller failure / error data : " + response.data)
+		console.log("Controller.js getPersonById() failure / error status : " + response.status) 
+		console.log("Controller.js getPersonById() failure / error data : " + response.data)
 		
 	})
 	
 
-	
+	// to edit existing person details
+	$scope.editPerson = function() {
+		
+		PersonService.updatePerson($scope.person).then(function(response) {
+			
+			console.log("Controller.js updatePerson() success status : " + response.status)
+			console.log("Controller.js updatePerson() success data : " + response.data)
+			$location.path('/getAllPersons')  ;
+			
+		} , function(response) {
+			
+			console.log("Controller.js updatePerson() failure / error status : " + response.status) 
+			console.log("Controller.js updatePerson() failure / error data : " + response.data)
+			
+		})
+	}
 	
 })
