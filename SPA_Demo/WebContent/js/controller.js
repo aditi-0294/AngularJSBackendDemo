f1628a79b190d6ef75436d879358c49e7dcb8677
Aditi <a.aditi0294@rediffmail.com>
2017-08-27 19:13:58 +0530

    second commit : function calls

--- SPA_Demo/WebContent/js/controller.js
+++ SPA_Demo/WebContent/js/controller.js
@@ -7,18 +7,22 @@
  * second function - for failure - returns other error code
  * 
  * $scope.persons - getAllPersons.html ( => {{persons}} ) , same in backend PersonDaoImpl.java
+ * 
+ * $scope.person - [{'first_name':'xyz','last_name':'abc'...}] - json data
+ * 
  */
 
 console.log("Controller.js started loading")
 
-myApp.controller('PersonController' , function($scope,PersonService) {
+myApp.controller('PersonController' , function($scope,PersonService,$location) {
 
+	
 	// to get all person details :
 	function getAllPersons() {
 	
 		PersonService.getAllPersons().then(function(response) {
 		
-	//	PersonService.getAllPersons.then(function(response) { - it is a statement : error - service is not defined 
+				//	PersonService.getAllPersons.then(function(response) { - it is a statement : error - service is not defined 
 		
 			$scope.persons = response.data ; // success codes - list of array details which retrieved from POSTMAN
 			console.log("Controller.js success status : " + response.status)
@@ -32,6 +36,26 @@ myApp.controller('PersonController' , function($scope,PersonService) {
 			
 		}) 
 
+	}
+	
+	
+	// to save person details :
+	$scope.saveInsertPerson = function() {
+		
+		PersonService.insertPersonDetails($scope.person).then(function(response) {
+			
+			$scope.person = response.data ; // success codes - list of array details which retrieved from POSTMAN
+			console.log("Controller.js insertPersonDetails() success status : " + response.status)
+			console.log("Controller.js insertPersonDetails() success data : " + response.data)
+			$location.path('/getAllPersons')
+			
+		} , function() {
+			
+			$scope.person = response.data ; // error / failure codes - list of array details which retrieved from POSTMAN
+			console.log("Controller.js insertPersonDetails() failure / error status : " + response.status)
+			console.log("Controller.js insertPersonDetails() failure / error data : " + response.data)
+			
+		})
 	}
 	
 	getAllPersons() ;
